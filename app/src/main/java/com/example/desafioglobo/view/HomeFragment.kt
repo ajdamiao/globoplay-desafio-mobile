c0d1ee0113be5064621aa7a1415686b3dc8a0249
damiao.junior <ajdamiaof@gmail.com>
2022-05-06 15:54:05 -0300

    Globo-2 | Listar Filmes

--- app/src/main/java/com/example/desafioglobo/view/HomeFragment.kt
+++ app/src/main/java/com/example/desafioglobo/view/HomeFragment.kt
@@ -5,11 +5,45 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.GridLayoutManager
 import com.example.desafioglobo.R
+import com.example.desafioglobo.adapter.MovieAdapter
+import com.example.desafioglobo.data.repository.MoviesRepository
+import com.example.desafioglobo.databinding.FragmentHomeBinding
+import com.example.desafioglobo.model.Movie
+import com.example.desafioglobo.viewmodel.HomeViewModel
 
 class HomeFragment : Fragment(R.layout.fragment_home) {
+    private lateinit var binding: FragmentHomeBinding
+    private lateinit var homeViewModel: HomeViewModel
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        binding = FragmentHomeBinding.bind(view)
+        homeViewModel = ViewModelProvider(this, HomeViewModel.HomeViewModelFactory(MoviesRepository())).get(HomeViewModel::class.java)
+
+        observerMoviesCallBack()
+        homeViewModel.callGetMovieList()
     }
+
+    private fun observerMoviesCallBack() {
+
+        homeViewModel.movieLiveData.observe(viewLifecycleOwner) { response ->
+            when(response) {
+                is Movie -> setupMovieRecyclerView(response)
+            }
+        }
+
+    }
+
+    private fun setupMovieRecyclerView(movie: Movie) {
+
+        binding.movieRecyclerView.layoutManager = GridLayoutManager(requireContext(), 3)
+        binding.movieRecyclerView.adapter = MovieAdapter(movie, requireContext())
+
+
+    }
+
 }
